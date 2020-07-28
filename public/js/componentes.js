6d047944fc3dc25ee8a0132ac49753822259a396
FIXDISC <acavieres@hotmail.com>
2020-07-27 20:44:42 -0400

    First commit

---
+++ public/js/componentes.js
@@ -0,0 +1,58 @@
+function busca(txt){
+    txt = txt.toString().trim();
+    document.getElementById("res").innerHTML =""
+    document.getElementById("txt").innerHTML=txt
+    var filtro = data.filter(
+        a=>a.description.toLowerCase().indexOf(txt)!==-1 || 
+        a.brand.toLowerCase().indexOf(txt)!==-1  ||
+        a.id==txt
+        );
+    //alert(filtro[0].id)
+    document.getElementById("count").innerHTML= filtro.length
+    if(filtro.length!=0){
+        muestra_res(filtro,txt);
+    }
+}
+
+function muestra_res(res,txt){
+    html="";
+    poli=[];
+    descuento = "";
+    precio_ant = "";
+    precio="";
+    res.map(function(elem){
+        if(!polindromo(txt)){
+            descuento="";
+            precio_ant = "";
+            precio = elem.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        }else{
+            descuento="<div class='descuento'><span>50%</span></div>";
+            precio_ant = "<div class='ant'>"+elem.price+"</div>";
+            precio0 = Math.round(Number(elem.price)/2);
+                precio = precio0.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".")
+        };
+        if(polindromo(elem.brand)){poli.push(elem.brand);}
+        html+=`
+        <div id="`+elem.id+`" class="prod">
+            <div class="img1">
+                <img src="https:\//`+elem.image+`" width="80">
+            </div>
+            <br>
+            <div class="brand">`+elem.brand+`&nbsp;&nbsp;</div>
+            <div class="desc">`+elem.description+`</div>
+            <br><br>
+            <div class="price">$ `+precio+`</div>
+            <div style='top:-10px;'>`+descuento+`</div>
+            <br><br>
+            <div>`+precio_ant+`</div>
+            </div>`
+        }
+    )
+    document.getElementById("res").innerHTML = html
+    console.log("POLI:  "+poli)
+}
+
+function polindromo(txt){
+    txt=txt.toString();
+    return txt == txt.split('').reverse().join('');
+}
