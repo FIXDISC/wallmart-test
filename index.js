6e94dcfa7bb9fbbd86b3b474cdb1d33dcf2f37e8
FIXDISC <acavieres@hotmail.com>
2020-07-27 21:16:02 -0400

    Add url testing

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.redirect('public/index.html')
 })
 
+app.get('/test', function(request, response) {
+  response.redirect('public/test/test_index.html')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
